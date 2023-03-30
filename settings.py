25cf41e713b3ec27d747b6fae07227e832c57a4c
Liderel <121626947+Liderel@users.noreply.github.com>
2023-03-30 15:25:32 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,8 @@
+valid_email = 'lorixa3158@lidely.com'
+valid_password = 'qwerty'
+
+invalid_email = 'asdfgvcxz@yandex.ru'
+invalid_password = 'asdfgh'
+
+
+invalid_auth_key = {'key': 'sdasdasd5s8s3e4'}
